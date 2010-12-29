35b9ac29fdcf0cb67518d688b35d1e479f4467f7
seth <seth@056512b4-9e07-413a-8608-9e8ffa60d165>
2010-12-29 22:08:08 +0000

    completed exercise 2.19
    git-svn-id: file:///media/raid/svn/sicp@6 056512b4-9e07-413a-8608-9e8ffa60d165

--- 2/2.19.scm
+++ 2/2.19.scm
@@ -1,17 +1,24 @@
-(define (count-change amount)
-  (cc amount 5))
-(define (cc amount kinds-of-coins)
-  (cond ((= amount 0) 1)
-        ((or (< amount 0) (= kinds-of-coins 0)) 0)
-        (else (+ (cc amount
-                     (- kinds-of-coins 1))
-                 (cc (- amount
-                        (first-denomination kinds-of-coins))
-                     kinds-of-coins)))))
-(define (first-denomination kinds-of-coins)
-  (cond ((= kinds-of-coins 1) 1)
-        ((= kinds-of-coins 2) 5)
-        ((= kinds-of-coins 3) 10)
-        ((= kinds-of-coins 4) 25)
-        ((= kinds-of-coins 5) 50)))
+(define us-coins (list 50 25 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
 
+(define (count-change amount)
+  (cc amount us-coins))
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else (+ (cc amount
+                     (except-first-denomination coin-values))
+                 (cc (- amount
+                        (first-denomination coin-values))
+                     coin-values)))))
+(define (first-denomination vals)
+  (car vals))
+(define (except-first-denomination vals)
+  (cdr vals))
+(define (no-more? vals)
+  (= (length vals) 0))
+
+;The order of the list does not affect the answer because all
+;combinations are tried regardless. Sorting the list with the
+;higher values fist does produce a "smaller" process becuase
+;it causes the branches to end more quickly.
