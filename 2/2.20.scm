aa923f4e544223fb20fe9e67b9e34335d40c3d96
seth <seth@056512b4-9e07-413a-8608-9e8ffa60d165>
2011-01-05 18:55:22 +0000

    improved readability of 2.2.scm
    git-svn-id: file:///media/raid/svn/sicp@8 056512b4-9e07-413a-8608-9e8ffa60d165

--- 2/2.20.scm
+++ 2/2.20.scm
@@ -1,7 +1,11 @@
 (define (same-parity n1 . nums)
-  (define (spr accum remain)
-    (cond ((null? remain) accum)
-          (else (cond ((equal? (even? n1) (even? (car remain))) (spr (append accum (list (car remain))) (cdr remain)))
-                      (else (spr accum (cdr remain)))))))
-  (spr (list) nums))
+  (define (sp-iter accum remain)
+    (cond 
+      ((null? remain) accum)
+      (else 
+        (cond 
+          ((equal? (even? n1) (even? (car remain)))
+            (sp-iter (append accum (list (car remain))) (cdr remain)))
+          (else (sp-iter accum (cdr remain)))))))
+  (sp-iter (list) nums))
 
