5d0a56028a0fc822439bb3f4d0cd7eae57b30fb5
seth <seth@056512b4-9e07-413a-8608-9e8ffa60d165>
2011-01-12 16:58:17 +0000

    completed exercise 2.24
    git-svn-id: file:///media/raid/svn/sicp@14 056512b4-9e07-413a-8608-9e8ffa60d165

---
+++ 2/2.24.scm
@@ -0,0 +1,26 @@
+(define (count-leaves x)
+  (cond ((null? x) 0)  
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves (car x))
+                 (count-leaves (cdr x))))))
+
+(count-leaves (list 1 (list 2 (list 3 4))))
+(list 1 (list 2 (list 3 4)))
+
+; (1 (2 (3 4)))
+; 
+; [.][.]-[.][/]
+;  |      |            
+; [1]    [.][.]-[.][/]
+;         |      |  
+;        [2]    [.][.]-[.][/]
+;                |      |
+;               [3]    [4]
+;
+;     *
+;    / \
+;   1   *
+;      / \
+;     2   *
+;        / \
+;       3   4
